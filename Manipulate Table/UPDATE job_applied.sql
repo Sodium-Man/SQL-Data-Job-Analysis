cc56e983da275532477c1d70377a20a347fd48bf
Soidum_Man <bhalaninaman7@gmail.com>
2025-01-03 09:31:23 +0530

    final CRUD

---
+++ Manipulate Table/UPDATE job_applied.sql
@@ -0,0 +1,22 @@
+UPDATE job_applied
+SET    contact_info = 'NamaN Bhalani'
+WHERE  job_id = 1;
+
+UPDATE job_applied
+SET    contact_info = 'Dinesh Chandimal'
+WHERE  job_id = 2;
+
+UPDATE job_applied
+SET    contact_info = 'Chris Gayle'
+WHERE  job_id = 3;
+
+UPDATE job_applied
+SET    contact_info = 'Sunil Gavaskar'
+WHERE  job_id = 4;
+
+UPDATE job_applied
+SET    contact_info = 'Big Show'
+WHERE  job_id = 5; 
+
+SELECT *
+FROM job_applied
